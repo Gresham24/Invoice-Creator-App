3752da8cc2670dedf379f1ce3969c86c33e5ea9f
Gresham24 <tembogresham@gmail.com>
2024-03-14 23:16:14 +0200

    initialised form print component

---
+++ src/components/FormPrint.jsx
@@ -0,0 +1,13 @@
+import { useContext } from 'react'
+import Form from './Form';
+
+function FormPrint() {
+
+  return (
+      <>
+          <p>Testing</p>
+      </>
+  );
+}
+
+export default FormPrint
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Form from './components/Form.jsx'
+// import FormPrint from './components/FormPrint.jsx'
 import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Form />
+    {/* <FormPrint /> */}
   </React.StrictMode>,
 )
