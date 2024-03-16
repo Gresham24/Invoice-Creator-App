201d4f63192fa3c50934e13f538f773ff34006bb
Gresham24 <tembogresham@gmail.com>
2024-03-16 13:25:34 +0200

    parent component to manage states and render form comp

---
+++ src/InvoiceApp.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import Form, { FormDataContext } from "./components/Form";
+import FormPrint from "./components/FormPrint";
+
+function InvoiceApp() {
+    const [formDetails, setFormDetails] = useState({
+        details: {},
+        items: [],
+    });
+
+    return (
+        <FormDataContext.Provider value={{ formDetails, setFormDetails }}>
+            <Form />
+            {/* <FormPrint /> */}
+        </FormDataContext.Provider>
+    );
+}
+
+export default InvoiceApp;
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Form from './components/Form.jsx'
-import FormPrint from "./components/FormPrint.jsx";
-import { FormDataContext } from "./components/Form.jsx";
 import "./styles/index.css";
+import InvoiceApp from "./InvoiceApp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        {/* <FormDataContext.Provider value={formValues}> */}
-            <Form />
-            {/* <FormPrint /> */}
-        {/* </FormDataContext.Provider> */}
+      <InvoiceApp/>
     </React.StrictMode>
 );
