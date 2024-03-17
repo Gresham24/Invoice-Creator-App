3a23472a51871dbcb04f3023fa19c2ee6c8d8ac3
Gresham24 <tembogresham@gmail.com>
2024-03-17 22:24:52 +0200

    styled print page component

--- src/components/FormPrint.jsx
+++ src/components/FormPrint.jsx
@@ -1,15 +1,30 @@
-import { useContext } from 'react'
-import {FormDataContext} from './Form';
+import { useContext } from "react";
+import styled from "styled-components";
+import { FormDataContext } from "./Form";
+import Form from "./Form";
+
+const StyledContainer = styled.div`
+    margin-top: 50px;
+    padding: 20px 32px;
+    border-radius: 15px;
+    background: #f8f9fa;
+    box-shadow: 0px 15px 48px 0px rgba(46, 47, 58, 0.08);
+    /* height: 882px; */
+`;
+
+const StyledPage = styled.div``;
 
 export default function FormPrint() {
     const { formValues } = useContext(FormDataContext);
-    console.log(formValues);
+
+
     return (
-        <>
-            <p>Details: {JSON.stringify(formValues.details)}</p>
-            <p>Items: {JSON.stringify(formValues.items)}</p>
-        </>
+        <StyledContainer>
+            <StyledPage>
+              <p>{}</p>
+                {/* <p>Details: {JSON.stringify(formValues.details)}</p> */}
+                {/* <p>Items: {JSON.stringify(formValues.items)}</p> */}
+            </StyledPage>
+        </StyledContainer>
     );
 }
-
-
